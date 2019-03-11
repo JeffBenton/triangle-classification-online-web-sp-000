d1ad2fdaa00d03593b9087f16404dec3074be644
Jeff Benton <sikletz@gmail.com>
2019-03-11 08:44:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,8 @@ class Triangle
   end
   
   def kind
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 > @side3 || @side1 + @side3 > @side2 || @side2 + @side3 > @side1
-      puts "here"
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
+      raise TriangleError
     end
     
     if @side1 == @side2 && @side1 == @side3
@@ -20,7 +20,7 @@ class Triangle
     end
   end
   
-  class TrangleError < StandardError
+  class TriangleError < StandardError
 
   end
 end
