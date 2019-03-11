5d371054417f6d3a3106fbcd7b5567320a66d305
Jeff Benton <sikletz@gmail.com>
2019-03-11 08:38:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,25 @@ class Triangle
   end
   
   def kind
+    if @side1 + @side2 > @side3 || @side1 + @side3 > @side2 || @side2 + @side3 > @side1
+      begin
+        raise TraingleException
+      rescue
+        TrangleException.message
+      end
+    end
+    
     if @side1 == @side2 && @side1 == @side3
       return :equilateral
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      return :scalene
+    else
+      return :isosceles
+    end
+  end
+  
+  class TriangleException > StandardException
+    def message
+    end
   end
 end
